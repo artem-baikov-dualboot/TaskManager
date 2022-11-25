54c074c36e44ac45a010a52072ad6c219ce4364b
Artem Baikov <artembaikov@Artems-MacBook-Pro.local>
2022-11-25 14:43:13 +0300

    [#8] Added associations and validations for user and task models

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :task do
     name { 'MyString' }
     description { 'MyText' }
-    author_id { 1 }
+    author factory: :manager
     assignee_id { 1 }
     state { 'MyString' }
     expired_at { '2022-11-24' }
