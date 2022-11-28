5989f01273d5a8e52e8ed1ea3a70a3cd498d55c6
Artem Baikov <artembaikov@Artems-MacBook-Pro.local>
2022-11-28 10:12:47 +0300

    Rewrote task state machine's transitions in one line

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,14 +1,19 @@
 class Task < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :assignee, class_name: 'User', optional: true
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :author, presence: true
+  validates :description, length: { maximum: 500 }
+
   state_machine initial: :new_task do
     event :develop do
-      transition new_task: :in_development
-      transition in_qa: :in_development
-      transition in_code_review: :in_development
+      transition [:new_task, :in_qa, :in_code_review] => :in_development
     end
 
     event :archive do
-      transition new_task: :archived
-      transition released: :archived
+      transition [:new_task, :released] => :archived
     end
 
     event :test do
@@ -27,12 +32,4 @@ class Task < ApplicationRecord
       transition ready_for_release: :released
     end
   end
-
-  belongs_to :author, class_name: 'User'
-  belongs_to :assignee, class_name: 'User', optional: true
-
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :author, presence: true
-  validates :description, length: { maximum: 500 }
 end
